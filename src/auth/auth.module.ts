e1655742899568be8e3e7ec3b4041eb198103f53
lazaro <lazaroalves12355@gmail.com>
2025-03-21 18:49:46 -0300

    feat: add imports ind auth.module

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -2,9 +2,23 @@ import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 import { PrismaService } from '../prisma/prisma.service';
+import { JwtStrategy } from '../strategy/jwt-auth.strategy';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { JwtModule } from '@nestjs/jwt';
 
 @Module({
-  providers: [AuthService, PrismaService],
+  imports:[
+    ConfigModule.forRoot(),
+    JwtModule.registerAsync({
+      imports: [ConfigModule],
+      useFactory: async (configService: ConfigService)=>({
+        secret:configService.get<string>('JWT_SECRET'),
+        signOptions: {expiresIn: '1D'},
+      }),
+      inject: [ConfigService]
+    })
+  ],
+  providers: [AuthService, PrismaService, JwtStrategy],
   controllers: [AuthController]
 })
 export class AuthModule {}
